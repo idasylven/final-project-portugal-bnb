9c977efa9cd007396c96e48fa71b3d39efb65bee
“[ida <[ida.t.sylven@gmail.com]>
2021-06-04 16:04:37 +0200

    Started working on backend mongoose model

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,19 @@ mongoose.connect(mongoUrl, {
 })
 mongoose.Promise = Promise
 
-const Guest = mongoose.model("Guest", {
+const GuestReservation = mongoose.model("GuestReservation", {
+  date: {
+    type: Number, 
+    required: true,
+  },
+  roomtype: {
+    type: String, 
+    required: true,
+  },
+  pax: {
+    type: Number, 
+    required: true,
+  },
   firstname: {
     type: String, 
     required: true,
@@ -30,11 +42,7 @@ const Guest = mongoose.model("Guest", {
   phonenumber: {
     type: Number, 
     required: true,
-  },
-  accessToken: {
-    type: String,
-    default: () => crypto.randomBytes(128).toString("hex"),
-  },
+  }
 });
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
@@ -44,13 +52,44 @@ const Guest = mongoose.model("Guest", {
 const port = process.env.PORT || 8080
 const app = express()
 
+app.post("/book", async (req, res) => {
+  const { date, roomtype, pax, firstname, lastname, email, phonenumber } = req.body
+
+  try {
+    const newGuestReservation = await new GuestReservation ({
+      date,
+      roomtype,
+      pax,
+      firstname,
+      lastname,
+      email,
+      phonenumber
+    }).save() 
+
+    res.json ({
+      success: true,
+      userID: newGuestReservation._id,
+      date: newGuestReservation.date,
+      roomtype: newGuestReservation.roomtype,
+      pax: newGuestReservation.pax,
+      firstname: newGuestReservation.firstname,
+      lastname: newGuestReservation.lastname,
+      email: newGuestReservation.email,
+      phonenumber: newGuestReservation.phonenumber
+    })
+
+  } catch (error) {
+    res.status(400).json({ message: "Sorry something went wrong!"})
+  }
+})
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
 
 // Start the server
@@ -58,3 +97,10 @@ app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
+// Is an access token needed?
+// accessToken: {
+// type: String,
+//  default: () => crypto.randomBytes(128).toString("hex"),
+// },
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,28 +1,30 @@
-// import React, { useState } from 'react'
-// import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-// import { GoThreeBars } from 'react-icons/go'
+import { GoThreeBars } from 'react-icons/go'
 
-// const Navbar = () => {
-//   const [state, setState] = useState({
-//     isOpen:false
-//   })
 
-//   handleToggle = () => {
-//     setState({isOpen:!state.isOpen})
-//   }
-//   return (
-//     <nav className="navbar">
-//       <div className= "nav-center">
-//         <div className= "nav-header">
-//           <button type="button" className="nav-btn" onClick={handleToggle}>
-//             <GoThreeBars/>
+const Navbar = () => {
+  const [state, setState] = useState({
+    isOpen:false
+  })
 
-//           </button>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
+  const handleToggle = () => {
+    setState({isOpen:!state.isOpen})
+  }
 
-// export default Navbar
+  return (
+    <nav className="navbar">
+      <div className= "nav-center">
+        <div className= "nav-header">
+          <button type="button" className="nav-btn" onClick={handleToggle}>
+            <GoThreeBars />
+          </button>
+        
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
