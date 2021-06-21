88649547a52f36ba88c5580d7a139662582d99a7
“[ida <[ida.t.sylven@gmail.com]>
2021-06-21 14:43:40 +0200

    Added css file for Book component

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -33,7 +33,7 @@ const CustomMap = () => {
     <div style={{ width: "70vw", height: "70vh"}}>
       <WrappedMap 
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
-        libraries=geometry,drawing,places&key=`} 
+        libraries=geometry,drawing,places&key=AIzaSyCOUeTGU1MZnvJOJQel83CGcExArJEW0i4`} 
         loadingElement={<div style={{ height: "100%" }}/>}
         containerElement={<div style={{ height: "100%" }}/>}
         mapElement={<div style={{ height: "100%" }}/>}
