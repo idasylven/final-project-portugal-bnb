237155976318bd86f83ea5d9f261b4008cdce868
“[ida <[ida.t.sylven@gmail.com]>
2021-06-22 13:56:23 +0200

    Another to try and push to update changes

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -2,16 +2,12 @@ import React from 'react'
 
 import Map from '../components/Map'
 import Footer from '../components/Footer'
-import HamburgerMenu from 'components/HamburgerMenu'
 
 import './About.css'
 
 const About = () => {
   return (
     <>
-      <div>
-       <HamburgerMenu />
-      </div>
       Hello from About!
       <div className="map-container">
       <Map />
